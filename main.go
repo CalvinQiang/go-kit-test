b87bc65c1c0284b157f8a8992228b209729255cd
Calvin <calvin@linwear.com>
2021-01-10 09:38:14 +0800

    新增健康检查接口

--- main.go
+++ main.go
@@ -17,6 +17,10 @@ func main() {
 	serverHandler := httpTransport.NewServer(endPoint, transport.DecodeUserRequest, transport.EncodeUserResponse)
 	r := goMux.NewRouter()                                                    // 这里我们引入了第三方路由
 	r.Methods("GET", "DELETE").Path(`/user/{uid:\d+}`).Handler(serverHandler) // 设置path格式的路由
+	r.Methods("GEt").Path("/health").HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "application/json")
+		_, _ = writer.Write([]byte(`{"status":"ok"}`))
+	})
 	// 监听端口，并且使用serverHandler处理随之而来的请求
 	_ = http.ListenAndServe(":8080", r)
 }
